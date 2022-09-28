039131dc3fe148bcd6cb011087a7b8699c157f96
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-28 15:46:36 +0900

    add:connect to database

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
-    //
+    public function index()
+    {
+        //全件取得
+        $values = Test::all();
+
+        // dd($values); //処理を止めて内容を確認できる
+        return view('tests.test', compact('values'));
+    }
 }
---
+++ resources/views/tests/test.blade.php
@@ -0,0 +1,6 @@
+test<br>
+
+@foreach($values as $value)
+{{ $value -> id }}<br>
+{{ $value -> test }}<br>
+@endforeach
