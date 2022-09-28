9188cb944228182f3463e017002117cf1d856999
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-28 16:19:10 +0900

    add:eloquent and querybuilder

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
-        //全件取得
+        //Eloquent
         $values = Test::all();
 
-        // dd($values); //処理を止めて内容を確認できる
+        $count = Test::count();
+        $first = Test::findOrFail(1);
+        $whereBBB = Test::where('test', '=', 'bbb')->get();
+
+        //クエリビルダ
+        $queryBuilder = DB::table('tests')->where('test', '=', 'bbb')
+        ->select('id','test')
+        ->get();
+
+        dd($values,$count, $first,$whereBBB,$queryBuilder); //処理を止めて内容を確認できる
         return view('tests.test', compact('values'));
     }
 }
