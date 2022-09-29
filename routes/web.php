869a64ddff531aa651296789f95ef120772ddfb6
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-09-29 14:51:06 +0900

    add:contactFromController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('tests/test',[TestController::class, 'index']);
+
+// Route::resource('contacts', ContactFormController::class);
 
 Route::get('/', function () {
     return view('welcome');
