09e2103aec78676ebe7b6f6a597872059a47ec96
MO-5995 <muu_goworld.1006@hotmail.co.jp>
2022-10-01 19:42:03 +0900

    fix:bug

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -33,7 +33,7 @@
                                             <div class="relative">
                                                 <label for="email"
                                                     class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                                <input type="email" id="email" name="name"
+                                                <input type="email" id="email" name="email"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             </div>
                                         </div>
@@ -53,7 +53,7 @@
                                         </div>
                                         <div class="p-2 w-full">
                                             <div class="relative">
-                                                <label for="url" class="leading-7 text-sm text-gray-600">年齢</label>
+                                                <label for="age" class="leading-7 text-sm text-gray-600">年齢</label>
                                                 <select name="age">
                                                     <option value="">選択してください</option>
                                                     <option value="1">〜19歳</option>
@@ -68,7 +68,7 @@
 
                                         <div class="p-2 w-full">
                                             <div class="relative">
-                                                <label for="message"
+                                                <label for="contact"
                                                     class="leading-7 text-sm text-gray-600">お問い合せ内容</label>
                                                 <textarea id="contact" name="contact"
                                                     class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
